ff8e1a39cc996f8498e9a30df80ae2ec9e303c65
Calvin Tran <calvinxtran@gmail.com>
2022-06-01 04:58:51 +0000

    added nav item to navbar for entries

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 var $photoUrl = document.querySelector('#photo_url');
 var $form = document.querySelector('#journal-form');
 var $ul = document.querySelector('ul');
+var $entriesBtn = document.querySelector('.entries-btn');
+var $entryForm = document.querySelector('.entry-form');
 
+// start@photoURL updates placeholder img
 $photoUrl.addEventListener('input', function () {
   document.getElementById('img-placeholder').setAttribute('src', $photoUrl.value);
 });
+// end@photoURL updates placeholder img
 
+// start@submit form inputs to data
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
 
@@ -21,7 +26,9 @@ $form.addEventListener('submit', function (event) {
 
   $form.reset();
 });
+// end@submit form inputs to data
 
+// start@dom creation for entries
 function journal(entry) {
   var $li = document.createElement('li');
   $ul.appendChild($li);
@@ -80,7 +87,9 @@ function journal(entry) {
 
   return $li;
 }
+// end@dom creation for entries
 
+// start@load dom content
 window.addEventListener('DOMContentLoaded', event => {
   for (var i = 0; i < data.entries.length; i++) {
     var journalEntries = journal(data.entries[i]);
@@ -88,3 +97,12 @@ window.addEventListener('DOMContentLoaded', event => {
     $ul.appendChild(journalEntries);
   }
 });
+// end@load dom content
+
+// start@nav entries
+$entriesBtn.addEventListener('click', function (e) {
+  if (e.target && e.target.matches('.entries-btn')) {
+    $entryForm.className = 'entry-form container hidden';
+  }
+});
+// end@nav entries
