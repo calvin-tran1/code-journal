0e31a4440490b71f258a534ed0ce059f5355b00b
Calvin Tran <calvinxtran@gmail.com>
2022-06-01 00:30:33 +0000

    defined function that returns dom tree of journal entry. #2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var $photoUrl = document.querySelector('#photo_url');
 var $form = document.querySelector('#journal-form');
+var $ul = document.querySelector('ul');
 
 $photoUrl.addEventListener('input', function () {
   document.getElementById('img-placeholder').setAttribute('src', $photoUrl.value);
@@ -20,3 +21,64 @@ $form.addEventListener('submit', function (event) {
 
   $form.reset();
 });
+
+function entry() {
+  var entryImg = data.entries.map(a => a.photoUrlValue);
+  var entryTitle = data.entries.map(a => a.titleValue);
+  var entryNotes = data.entries.map(a => a.notesValue);
+
+  var $li = document.createElement('li');
+  $ul.appendChild($li);
+
+  // start@image dom creation
+  var $divRowEntries = document.createElement('div');
+  $divRowEntries.classList.add('row', 'entries');
+  $li.appendChild($divRowEntries);
+
+  var $divColumnHalfEntriesImg = document.createElement('div');
+  $divColumnHalfEntriesImg.classList.add('column-half', 'entries', 'img');
+  $divRowEntries.appendChild($divColumnHalfEntriesImg);
+
+  var $entryImg = document.createElement('img');
+  $entryImg.classList.add('entry-img');
+  $divColumnHalfEntriesImg.appendChild($entryImg);
+  // end@image dom creation
+
+  // start@title dom creation
+  var $divColumnHalfEntriesTitleNotes = document.createElement('div');
+  $divColumnHalfEntriesTitleNotes.classList.add('column-half', 'entries', 'title-notes');
+  $divRowEntries.appendChild($divColumnHalfEntriesTitleNotes);
+
+  var $divRowTitle = document.createElement('div');
+  $divRowTitle.classList.add('row', 'title');
+  $divColumnHalfEntriesTitleNotes.appendChild($divRowTitle);
+
+  var $divColumnFullEntriesTitle = document.createElement('div');
+  $divColumnFullEntriesTitle.classList.add('column-full', 'entries', 'title');
+  $divRowTitle.appendChild($divColumnFullEntriesTitle);
+
+  var $pTitle = document.createElement('p');
+  $pTitle.classList.add('title');
+  $divColumnFullEntriesTitle.appendChild($pTitle);
+  // end@title dom creation
+
+  // start@notes dom creation
+  var $divRowNotes = document.createElement('div');
+  $divRowNotes.classList.add('row', 'notes');
+  $divColumnHalfEntriesTitleNotes.appendChild($divRowNotes);
+
+  var $divColumnFullEntriesNotes = document.createElement('div');
+  $divColumnFullEntriesNotes.classList.add('column-full', 'entries', 'notes');
+  $divRowNotes.appendChild($divColumnFullEntriesNotes);
+
+  var $pNotes = document.createElement('p');
+  $pNotes.classList.add('notes');
+  $divColumnFullEntriesNotes.appendChild($pNotes);
+  // end@notes dom creation
+
+  $entryImg.setAttribute('src', entryImg);
+  $pTitle.append(entryTitle);
+  $pNotes.append(entryNotes);
+}
+
+entry();
